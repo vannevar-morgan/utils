85d3a612e4002f974ddbe805dda3001beb67e976
vannevar-morgan <vnnvr.mrgn@gmail.com>
2017-09-10 02:32:51 -0400

    refactored some functions

--- rehash/src/rehash.py
+++ rehash/src/rehash.py
@@ -24,20 +24,17 @@ def rehash_png(png_filename):
     reader = png.Reader(png_filename)
     [width, height, pix, metadata] = reader.read()
     writer = png.Writer(**metadata)
-    writer.set_text({REHASH:comment_text})
+    writer.set_text({"rehash":comment_text})
     with open(png_filename, "wb") as out_file:
         # png.Writer(**metadata).write(out_file, pix)
         writer.write(out_file, pix)
-    # except:
-    #     print(MESSAGE_FILE_READ_ERROR)
-    #     sys.exit(1)
 
 
 def rehash_jpg(jpg_filename):
     """
     Write ExifIFD.UserComment in jpg exif to change the file hash    
     """
-    exif_IFD = {piexif.ExifIFD.UserComment: str.encode("\0\0\0\0\0\0\0\0" + REHASH + COMMENT_SEP + make_rtext())}
+    exif_IFD = {piexif.ExifIFD.UserComment: str.encode("\0\0\0\0\0\0\0\0rehash: " + make_rtext())}
     # exif_IFD = {piexif.ImageIFD.ImageDescription: "abcdefghijklmnopqrstuvwxyz0123456789"}
     exif_dict = {"Exif":exif_IFD}
     exif_bytes = piexif.dump(exif_dict)
@@ -48,6 +45,8 @@ def check_gif_vs_support(gif_filename):
     """
     Confirm gif file version is "GIF89a" so comment blocks are supported
     """
+    GIF_HEADER_BYTES = 6
+    MESSAGE_GIF_VERSION_ERROR = "gif version is not supported (gif version must be \"GIF89a\" to support comment blocks)... try updating gif file version?"
     with open(gif_filename, "rb") as in_file:
         if in_file.read(GIF_HEADER_BYTES) != b"GIF89a":
             return False
@@ -60,7 +59,10 @@ def get_cblock_offset(gif_filename):
     Return the offset to the first position where writing a comment block is valid.
     This is immediately after the Global Color Table, if present.
     """
-    offset = GIF_HEADER_BYTES + GIF_LSD_BYTES # Header bytes (6) + Logical Screen Descriptor (7)
+    GIF_HEADER_BYTES = 6
+    GIF_LSD_BYTES = 7 # 7 bytes, logical screen descriptor
+    GIF_PF_OFFSET = 10 # offset bytes to the packed field byte, 6 byte header + 4 bytes (logical screen width & height)
+    offset = GIF_HEADER_BYTES + GIF_LSD_BYTES # Header bytes + Logical Screen Descriptor
     with open(gif_filename, "rb") as gif_file:
         gif_file.seek(GIF_PF_OFFSET)
         packed_field = gif_file.read(1)
@@ -79,10 +81,10 @@ def insert_gif_comment(gif_filename):
     Insert comment block in the specified gif file
     """
     if not check_gif_vs_support(gif_filename):
-        print(MESSAGE_GIF_VERSION_ERROR)
+        # print(MESSAGE_GIF_VERSION_ERROR)
         return
     else:
-        comment = REHASH + COMMENT_SEP + make_rtext()
+        comment = "rehash: " + make_rtext()
         with open(gif_filename, "r+b") as gif_file:
             comment_offset = get_cblock_offset(gif_filename)
             gif_file.seek(comment_offset)
@@ -102,10 +104,11 @@ def rehash_gif(gif_filename):
     """
     Rehash specified gif file
     """
+    MESSAGE_GIF_INSERT_ERROR = "error occurred while inserting comment block in gif file"
     try:
         insert_gif_comment(gif_filename)
     except:
-        print(MESSAGE_GIF_INSERT_ERROR)
+        # print(MESSAGE_GIF_INSERT_ERROR)
         return
 
 
@@ -120,7 +123,7 @@ def rehash_ebml(ebml_filename):
     """
     Write a tag segment at the end of the specified ebml container
     """
-    comment = REHASH + COMMENT_SEP + make_rtext()
+    comment = "rehash: " + make_rtext()
     ebml_id_tags = b"\x12\x54\xc3\x67"
     ebml_id_tag = b"\x73\x73"
     ebml_id_targets = b"\x63\xc0"
@@ -144,71 +147,78 @@ def rehash_ebml(ebml_filename):
         ebml_file.write(tag_segment)
 
 
+def do_rehash(filename, ext):
+    """
+    Call the appropriate rehash function for the file extension.
+    """
+    if ext == "png":
+        rehash_png(filename)
+    elif ext == "jpg":
+        rehash_jpg(filename)
+    elif ext == "jpeg":
+        rehash_jpg(filename)
+    elif ext == "gif":
+        rehash_gif(filename)
+    elif ext == "webm":
+        rehash_ebml(filename)
+    elif ext == "mkv":
+        rehash_ebml(filename)
+    elif ext == "mp4":
+        rehash_mp4(filename)
+    else:
+        pass
 
-REHASH = "rehash"
-COMMENT_SEP = ": "
-SPACE = " "
-MESSAGE_NO_FILE = "is not a valid file."
-MESSAGE_FILE_EXT_UNKNOWN = "couldn't determine the file type... check file extension?"
-MESSAGE_FILE_EXT_UNSUPPORTED = "file type is unsupported."
-MESSAGE_SUPPORTED_FILETYPES = "Supported file types:\n.png"
-MESSAGE_FILE_READ_ERROR = "error reading file."
-MESSAGE_FILE_WRITE_ERROR = "error writing file."
-MESSAGE_GIF_VERSION_ERROR = "gif version is not supported (gif version must be \"GIF89a\" to support comment blocks)... try updating gif file version?"
-MESSAGE_GIF_INSERT_ERROR = "error occurred while inserting comment block in gif file"
-MESSAGE_GENERAL_UNKNOWN = "unknown error occurred :("
 
-GIF_HEADER_BYTES = 6
-GIF_LSD_BYTES = 7 # 7 bytes, logical screen descriptor
-GIF_PF_OFFSET = 10 # offset bytes to the packed field byte, 6 byte header + 4 bytes (logical screen width & height)
-
-
-parser = argparse.ArgumentParser(description="Change the hash of a file.")
-parser.add_argument("-o", help="overwrite (write output to input file)", action="store_true")
-parser.add_argument("filename", help="change the hash of this file")
-
-args = parser.parse_args()
-
-target = args.filename
+def is_filetype_supported(filename):
+    """
+    Check if a file type is valid and supported by rehash.
+    If true, return the file extension.
+    """
+    MESSAGE_NO_FILE = "is not a valid file."
+    MESSAGE_FILE_EXT_UNKNOWN = "couldn't determine the file type... check file extension?"
+    MESSAGE_FILE_EXT_UNSUPPORTED = "file type is unsupported."
     
-if not os.path.isfile(target):
-    print(target + SPACE + MESSAGE_NO_FILE)
-    sys.exit(1)
+    if not os.path.isfile(filename):
+        # print(filename + " " + MESSAGE_NO_FILE)
+        return None
+    
+    # get the filename file extension and compare to supported extensions
+    FILETYPES = ("png", "jpg", "jpeg", "gif", "webm", "mkv")
+    
+    ext_split = os.path.splitext(filename)
+    file_ext = ext_split[1]
+    if (not file_ext) or file_ext == os.path.extsep:
+        # print(MESSAGE_FILE_EXT_UNKNOWN)
+        return None
+    file_ext = (file_ext[1:]).lower() # remove extension separator, make extension lower
+    if file_ext not in FILETYPES:
+        # print(MESSAGE_FILE_EXT_UNSUPPORTED)
+        return None
+    # filetype is known and supported
+    return file_ext
+    
 
-# get the target file extension and compare to supported extensions
-FILETYPES = ("png", "jpg", "jpeg", "gif", "webm", "mkv")
+def main(argv):
+    """
+    """
+    parser = argparse.ArgumentParser(description="Change the hash of a file.")
+    parser.add_argument("-o", help="overwrite (write output to input file)", action="store_true")
+    parser.add_argument("filename", help="change the hash of this file")
+    
+    args = parser.parse_args(argv[1:])
+    
+    filename = args.filename
+    file_ext = is_filetype_supported(filename)
+    if not file_ext:
+        sys.exit(1)
 
-ext_split = os.path.splitext(target)
-target_ext = ext_split[1]
-if (not target_ext) or target_ext == os.path.extsep:
-    print(MESSAGE_FILE_EXT_UNKNOWN)
-    sys.exit(1)
-target_ext = (target_ext[1:]).lower() # remove extension separator, make extension lower
-if target_ext not in FILETYPES:
-    print(MESSAGE_FILE_EXT_UNSUPPORTED)
-    sys.exit(1)
+    if not args.o:
+        new_filename = "rehash_" + filename
+        copyfile(filename, new_filename)
+        filename = new_filename
+    
+    do_rehash(filename, file_ext)
 
-# filetype is known and supported
-#print("filetype is: " + target_ext) # change this to logging
 
-if not args.o:
-    new_filename = REHASH + "_" + target
-    copyfile(target, new_filename)
-    target = new_filename
-
-if target_ext == "png":
-    rehash_png(target)
-elif target_ext == "jpg":
-    rehash_jpg(target)
-elif target_ext == "jpeg":
-    rehash_jpg(target)
-elif target_ext == "gif":
-    rehash_gif(target)
-elif target_ext == "webm":
-    rehash_ebml(target)
-elif target_ext == "mkv":
-    rehash_ebml(target)
-elif target_ext == "mp4":
-    rehash_mp4(target)
-else:
-    pass
+if __name__ == "__main__":
+    main(sys.argv)
